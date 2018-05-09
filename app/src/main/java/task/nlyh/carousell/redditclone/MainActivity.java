9891a26fdd02f2a6e990d891f7e33fbea2fd3ec3
Nicholas Lee <nicholaslyh@hotmail.com>
2018-05-09 21:55:13 +0800

    Code cleanup - Removing Redundants Codes.

--- app/src/main/java/task/nlyh/carousell/redditclone/MainActivity.java
+++ app/src/main/java/task/nlyh/carousell/redditclone/MainActivity.java
@@ -10,8 +10,6 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 
 import org.greenrobot.eventbus.EventBus;
@@ -19,8 +17,6 @@ import org.greenrobot.eventbus.Subscribe;
 import org.greenrobot.eventbus.ThreadMode;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -50,28 +46,6 @@ public class MainActivity extends AppCompatActivity {
         update_ui();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * This function is to update the homescreen UI's recycler view
      * */
@@ -83,6 +57,9 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setAdapter(adapter);
     }
 
+    /**
+     * This Event Subscriber is listening for any UI changes
+     * */
     @Subscribe(threadMode = ThreadMode.MAIN)
     public void onMessageEvent (UpdateUiEvent event) {
         topicsList = event.getTopicsList();
